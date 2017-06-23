e02a3537f4b97521cbf5afe5b38746f8ee9f938c
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-23 15:19:15 -0600

    refactoring piece done but not functioning

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,17 +3,24 @@ require 'sqlite3'
 
 class Task
 
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
-    @database    = SQLite3::Database.new('db/task_manager_development.db')
-    @database.results_as_hash = true
+    # @database    = SQLite3::Database.new('db/task_manager_development.db')
+    # @database.results_as_hash = true
+    @id          = task_params["id"] if task_params["id"]
   end
 
   def save
     @database.execute("INSERT INTO tasks (title, description) VALUES (?, ?);", @title, @description)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
   end
 
   def self.all
@@ -31,5 +38,5 @@ class Task
     task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
     Task.new(task)
   end
-  
+
 end
