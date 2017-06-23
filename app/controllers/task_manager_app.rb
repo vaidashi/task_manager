4ba890920a028161e70068d8387f2c275152f314
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-23 14:07:02 -0600

    created task class

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,3 +1,5 @@
+require_relative '../models/task.rb'
+
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
@@ -8,5 +10,15 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks' do
     @tasks = ["task1", "task2", "task3"]
     erb :index
-  end 
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  post '/tasks' do
+    task = Task.new(params[:task])
+    task.save
+    redirect '/tasks'
+  end
 end
